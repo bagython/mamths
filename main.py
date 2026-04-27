f103d75f932e79adfb8d5df3addacb8ecf6ff228
TheScreechingBagel <bagel@vlyn.org>
2026-04-27 18:44:11 +0200

    getting quirky with it

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+from inspect import getsource
+from math import ceil, log2
 from typing import Callable
 
 
-def f(x):
+def forg(x):
     return x**3 - 27
 
 
@@ -10,13 +12,21 @@ def fof(x):
 
 
 def rootfinder(
-    function: Callable[[float], float], xvals: tuple[float, float], error: float
-):
-    a, b = xvals
-    c = (a + b) / 2
+    function: Callable[[float], float], interval: tuple[float, float], error: float
+) -> float:
+    a, b = interval
+
+    codigits = len(
+        str(bisection_worst_case_steps(interval, error))
+    )  # pad iteration number
+
     count = 0
     while b - a > error:
         c = (a + b) / 2
+
+        if function(c) == 0:
+            break
+
         if function(c) * function(a) < 0:
             b = c
         elif function(c) * function(b) < 0:
@@ -25,15 +35,34 @@ def rootfinder(
             raise Exception(
                 "erm the interval is not such that there is a singular root"
             )
-        print(count, c, function(c))
+        print(f"{count:<{codigits}} {c:.{16}f}", function(c))
         count += 1
-    return c
+    return (a + b) / 2
+
+
+def bisection_worst_case_steps(interval: tuple[float, float], epsilon: float) -> int:
+    a, b = interval
+    return ceil(log2((b - a) / epsilon))
 
 
 def main():
     epsilon = 0.0000000000001
-    print(rootfinder(f, (0, 5), epsilon))
-    print(rootfinder(fof, (-1, 1), epsilon))  # nuh uh
+    print(rootfinder(forg, (0, 5), epsilon))
+    # print(rootfinder(fof, (-1, 1), epsilon))  # nuh uh
+
+    print("homework: ")
+    eps = 0.01
+    interval = (1, 5)
+
+    def f(x):
+        return (x**2) - 4
+
+    print(getsource(f))  # :D
+    print(
+        f"expected worst case iterations: {bisection_worst_case_steps(interval, eps)}"
+    )
+    print("iteration | root approximation | f(c) value")
+    print("root:", rootfinder(f, interval, eps))
 
 
 if __name__ == "__main__":
